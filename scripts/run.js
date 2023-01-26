f8e90c844940a4da2c6a917c187664cffbd19656
Anupriya Lathey <anupriyalathey@gmail.com>
2023-01-26 05:46:59 +0530

    added total waves function

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,6 @@
 // The Hardhat Runtime Environment, or HRE for short, is an object containing all the functionality that Hardhat exposes when running a task, test or script. In reality, Hardhat is the HRE.
-
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   // This will compile our contract and generate the necessary files we need to work with our contract under the artifacts directory.
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
@@ -13,6 +13,19 @@ const main = async () => {
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const firstWaveTxn = await waveContract.wave(); //("This is wave #1");
+  await firstWaveTxn.wait(); // Wait for the transaction to be mined
+
+  await waveContract.getTotalWaves();
+
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave(); //("This is wave #2");
+  await secondWaveTxn.wait(); // Wait for the transaction to be mined
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
